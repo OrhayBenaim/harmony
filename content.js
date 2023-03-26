c30430758a8cbf42f77926eaf5d47680df2b63db
Orhay Benaim <orhay.benaim@sunbit.com>
2023-03-26 15:16:57 +0300

    added time selects

--- content.js
+++ content.js
@@ -5,16 +5,16 @@ const count = () => {
   return missings.length;
 };
 
-const fill = () => {
+const fill = (time) => {
   missings.forEach((missing) => {
     missing.querySelector('[aria-describedby="NameTrnFnc_startAW"] input').click();
-    missing.querySelector('[aria-describedby="NameTrnFnc_startAW"] input[name="Time_startAW"]').value = "09:00";
+    missing.querySelector('[aria-describedby="NameTrnFnc_startAW"] input[name="Time_startAW"]').value = `${time.start}:00`;
     missing.querySelector('[aria-describedby="NameTrnFnc_startAW"] input[name="Time_startAW"]').dispatchEvent(new Event("change", { isTrusted: true, bubbles: true }));
     missing.querySelector('[aria-describedby="NameTrnFnc_startAW"] input[name="Code_startAW"]').click();
     document.querySelector('#widgetCombo-list ul[role="listbox"] li[role="option"][data-offset-index="0"]').click();
 
     missing.querySelector('[aria-describedby="NameTrnFnc_endAW"] input').click();
-    missing.querySelector('[aria-describedby="NameTrnFnc_endAW"] input[name="Time_endAW"]').value = "18";
+    missing.querySelector('[aria-describedby="NameTrnFnc_endAW"] input[name="Time_endAW"]').value = time.end;
     missing.querySelector('[aria-describedby="NameTrnFnc_endAW"] input[name="Time_endAW"]').dispatchEvent(new Event("change", { isTrusted: true, bubbles: true }));
     missing.querySelector('[aria-describedby="NameTrnFnc_endAW"] input[name="Code_endAW"]').click();
     document.querySelector('#widgetCombo-list ul[role="listbox"] li[role="option"][data-offset-index="0"]').click();
@@ -25,9 +25,9 @@ const fill = () => {
   }
 };
 
-chrome.runtime.onMessage.addListener(({ message }, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(({ message, time }, sender, sendResponse) => {
   if (message === "fill") {
-    fill();
+    fill(time);
   }
 
   sendResponse(count());
--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const urlRegex = /^https?:\/\/(?:[^./?#]+\.)?harmony\.synel\.co\.il/;
 
+const startSelect = document.querySelector("#start");
+const endSelect = document.querySelector("#end");
+
+startSelect.addEventListener("change", (e) => {
+  const start = e.target.selectedOptions[0].value;
+  time.start = start;
+});
+
+endSelect.addEventListener("change", (e) => {
+  const end = e.target.selectedOptions[0].value;
+  time.end = end;
+});
+
 const updateCount = (length) => {
   const countElement = document.querySelector("#count");
   countElement.innerHTML = length;
 };
 
+const getStart = () => {
+  return startSelect.selectedOptions[0].value;
+};
+
+const getEnd = () => {
+  return endSelect.selectedOptions[0].value;
+};
+
+let time = {
+  start: getStart(),
+  end: getEnd(),
+};
 document.querySelector("#autoFill").addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-  const response = await chrome.tabs.sendMessage(tab.id, { message: "fill" });
+  const response = await chrome.tabs.sendMessage(tab.id, { message: "fill", time });
   updateCount(response);
 });
 
